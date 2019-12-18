2b606f46f87ffd0ffe65a191d666ec7ae6b58fac
Alistair Burrowes <alistair.burrowes@rea-group.com>
2019-12-18 12:41:56 +1100

    Done 3 lens virtual fields

--- src/Chapter3/LensVirtualFields.hs
+++ src/Chapter3/LensVirtualFields.hs
@@ -1,12 +1,24 @@
 module Chapter3.LensVirtualFields where
 
 import           Control.Lens.TH                ( makeLenses )
+import           Control.Lens.Type              ( Lens' )
+import           Control.Lens.Lens              ( lens )
+import           Data.List                      ( intercalate )
 
 data User = User
   { _firstName :: String
   , _lastName :: String
-  , _username :: String
   , _email :: String
   }
 
 makeLenses ''User
+
+username :: Lens' User String
+username = lens getter setter
+ where
+  getter (User fn sn _) = fn <> " " <> sn
+  setter (User _ _ email) fullName =
+    let nameWords = words fullName
+        firstName = head nameWords
+        lastName  = unwords (tail nameWords)
+    in  User firstName lastName email
