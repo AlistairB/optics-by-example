43a12037c73a3d03524f2ed68cb59f56a81aa614
Alistair Burrowes <alistair.burrowes@gmail.com>
2019-12-22 09:18:46 +1100

    Prep lens composition questions

---
+++ src/Chapter4/LensComposition.hs
@@ -0,0 +1,7 @@
+module Chapter4.LensComposition where
+
+import           Control.Lens.TH                ( makeLenses )
+import           Control.Lens.Type              ( Lens )
+import           Control.Lens.Lens              ( lens )
+
+-- 1. Fill in the blank with the appropriate composition of tuple lenses in the following statement:
